6a36658aab6eaae7ce4f69192243043fb75173c2
thedudxo <isaacboon@gmail.com>
2019-12-18 17:36:44 +1300

    curvy blender

---
+++ Assets/RotateEachFrame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateEachFrame : MonoBehaviour
+{
+    [SerializeField] Vector3 rotatePerFrame = new Vector3 (0,0,0);
+
+    void Update()
+    {
+        transform.Rotate(rotatePerFrame);
+    }
+}
---
+++ Assets/TranslateSinEachFrame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TranslateSinEachFrame : MonoBehaviour
+{
+    [SerializeField] Vector3 speed = new Vector3();
+    [SerializeField] Vector3 intensity = new Vector3();
+    Vector3 startPosition;
+    Vector3 sin = new Vector3();
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        sin += speed;
+
+        transform.position = new Vector3(
+            startPosition.x + Mathf.Sin(sin.x) * intensity.x,
+            startPosition.y + Mathf.Sin(sin.y) * intensity.y,
+            startPosition.z + Mathf.Sin(sin.z) * intensity.z
+            );
+    }
+}
