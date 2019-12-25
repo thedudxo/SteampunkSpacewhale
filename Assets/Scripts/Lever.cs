54bb6d92ac56110780c9b9412c344ca0c5916ec4
thedudxo <isaacboon@gmail.com>
2019-12-25 18:01:38 +1300

    greybox mostly done

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour, Interactable
+{
+    [SerializeField] Animator leverAnimator;
+    [SerializeField] Animator blenderAnimator;
+
+    public void Interact()
+    {
+        leverAnimator.SetTrigger("Lever");
+        blenderAnimator.SetTrigger("stop");
+    }
+}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField] public KeyCode interactKey;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.GetComponent<Interactable>() != null &&
+            Input.GetKeyDown(interactKey))
+        {
+            other.gameObject.GetComponent<Interactable>().Interact();
+        }
+        
+    }
+}
